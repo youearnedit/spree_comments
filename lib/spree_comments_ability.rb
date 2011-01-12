3810bda371c13f094ac477c1ee88d5a4a4a99325
Steven Jenkins <steven.jenkins@gmail.com>
2011-01-12 11:09:12 -0500

    for bootstrapping, we need to ignore CanCan abilities so that rake, rails console, etc, can load

--- lib/spree_comments_ability.rb
+++ lib/spree_comments_ability.rb
@@ -1,6 +1,11 @@
 class SpreeCommentsAbility
-  can :create, Comment
-  can :read, Comment
-  can :update, Comment
-  can :delete, Comment
+  include CanCan::Ability
+  # Only set up CanCan if table is already present (needed for
+  # being able to run migrations and bootstrap)
+  if  ActiveRecord::Base.connection.tables.include?('comments')
+    can :create, Comment
+    can :read, Comment
+    can :update, Comment
+    can :delete, Comment
+  end
 end
