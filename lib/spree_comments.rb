06ca80177c8a5e0947d7fbc242dc111e6b458a1b
Trung Lê <joneslee85@gmail.com>
2011-08-31 12:49:11 +1000

    Cleaup trailing spaces and tabs

--- lib/spree_comments.rb
+++ lib/spree_comments.rb
@@ -7,31 +7,31 @@ module SpreeComments
 
     config.autoload_paths += %W(#{config.root}/lib)
 
-	  def self.activate
-	    Order.class_eval do
-	      acts_as_commentable
-	    end
-	
-	    Shipment.class_eval do
-	      acts_as_commentable
-	    end
-	
-	    Admin::OrdersController.class_eval do
-	      def comments
-	        load_object
-	        @comment_types = CommentType.find(:all, :conditions => {:applies_to => "Order"} )
-	      end
-	    end
-	
-	    Admin::ShipmentsController.class_eval do
-	      def comments
-	        load_object
-	        @comment_types = CommentType.find(:all, :conditions => {:applies_to => "Shipment"} )
-	      end
-	    end
-	
-	  end
+    def self.activate
+      Order.class_eval do
+        acts_as_commentable
+      end
+
+      Shipment.class_eval do
+        acts_as_commentable
+      end
+
+      Admin::OrdersController.class_eval do
+        def comments
+          load_object
+          @comment_types = CommentType.find(:all, :conditions => {:applies_to => "Order"} )
+        end
+      end
+
+      Admin::ShipmentsController.class_eval do
+        def comments
+          load_object
+          @comment_types = CommentType.find(:all, :conditions => {:applies_to => "Shipment"} )
+        end
+      end
+
+    end
 
     config.to_prepare  &method(:activate).to_proc
-	end
+  end
 end
