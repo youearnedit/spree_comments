57ed063e72971e7cfa1e5ab6a91b691da4a1b087
Jeff Squires <jeff.squires@gmail.com>
2011-09-18 17:38:21 -0400

    fix deface insertion point

--- lib/spree_comments_hooks.rb
+++ lib/spree_comments_hooks.rb
@@ -1,7 +1,7 @@
 class SpreeCommentsHooks < Spree::ThemeSupport::HookListener
   Deface::Override.new(:virtual_path => "admin/shared/_order_tabs",
                        :name => "converted_admin_order_tabs_472794197",
-                       :insert_after => "[data-hook='admin_order_tabs'], #admin_order_tabs[data-hook]",
+                       :insert_bottom => "[data-hook='admin_order_tabs'], #admin_order_tabs[data-hook]",
                        :partial => "admin/orders/tab",
                        :disabled => false)
 
