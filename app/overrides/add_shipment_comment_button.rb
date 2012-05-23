4b6e6208b1100f6b14df1f2cc3b655b3dcaf7b4d
Mario Visic <mario@mariovisic.com>
2012-05-23 17:14:16 +0800

    Fix the path of the shipment comment ovveride.

--- app/overrides/add_shipment_comment_button.rb
+++ app/overrides/add_shipment_comment_button.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "admin/shipments/edit",
+Deface::Override.new(:virtual_path => "spree/admin/shipments/edit",
                      :name => "converted_admin_shipment_edit_buttons_233418828",
                      :insert_after => "[data-hook='admin_shipment_edit_buttons'], #admin_shipment_edit_buttons[data-hook]",
                      :partial => "spree/admin/shipments/button",
