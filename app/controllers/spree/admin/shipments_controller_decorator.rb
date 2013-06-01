381e9118f48d4e504ca31e3b18f1220da3d9336e
robertp82 <robwp21@gmail.com>
2013-06-01 11:53:50 -0400

    Fix namespace for Shipments Controller

--- app/controllers/spree/admin/shipments_controller_decorator.rb
+++ app/controllers/spree/admin/shipments_controller_decorator.rb
@@ -1,4 +1,4 @@
-Spree::Admin::ShipmentsController.class_eval do
+Spree::Api::ShipmentsController.class_eval do
   def comments
     shipment
     @comment_types = Spree::CommentType.where(:applies_to => "Shipment")
