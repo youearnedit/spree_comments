017c928158625f8ab89176acce7f315db425686a
Ramón Roche <mrpollo@gmail.com>
2014-01-16 16:46:22 -0800

    Remove old unused shipment overrides
    Fixes #34

--- app/overrides/add_shipment_comment_button.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/admin/shipments/edit",
-                     :name => "converted_admin_shipment_edit_buttons_233418828",
-                     :insert_after => "code[erb-silent]:contains('content_for :page_actions')",
-                     :partial => "spree/admin/shipments/button",
-                     :disabled => false)
